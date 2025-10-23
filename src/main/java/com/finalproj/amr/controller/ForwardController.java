69f31e6e05fc824ea801241cd0f7c6e834a6fd68
MonirithL <lengmonirith@gmail.com>
2025-10-23 20:35:02 +0700

    final forwarder

--- src/main/java/com/finalproj/amr/controller/ForwardController.java
+++ src/main/java/com/finalproj/amr/controller/ForwardController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ForwardController {
 
-    @RequestMapping(value = "/{[path:[^\\.]*}")
+    @RequestMapping(value = "/{path:[^\\.]*}")
     public String forward() {
         // Forward to React's index.html
         return "forward:/index.html";
