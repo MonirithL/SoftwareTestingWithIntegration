9a7a32660bf6e575111a7d6f7ebb298b76989159
MonirithL <lengmonirith@gmail.com>
2025-10-23 14:35:40 +0700

    corss

--- src/main/java/com/finalproj/amr/config/WebConfig.java
+++ src/main/java/com/finalproj/amr/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // allow all endpoints
-                        .allowedOriginPatterns("*")
+                        .allowedOriginPatterns("https://hangmyself.fun", "https://www.hangmyself.fun")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);  // now valid
